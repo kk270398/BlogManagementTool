c00c79ecab6c7432b00f05e3119fec5f0a812f92
Kanishk Chandrayan <INT805@mmt.com>
2019-06-24 18:20:19 +0530

    first commit

---
+++ bmtool/src/main/java/com/kk/spring/boot/BmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.kk.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmtoolApplication.class, args);
+	}
+
+}
